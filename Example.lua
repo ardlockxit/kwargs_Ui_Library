638cc861965368e3ae22e2f422e26af858054a23
ardlockxit <contacarx6@gmail.com>
2024-07-07 14:52:55 -0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,3 @@
--- this is an example for the script, use this to make your own! (Might be adding custom Themes)
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/7yhx/kwargs_Ui_Library/main/source.lua"))()
 
 local UI = Lib:Create{
@@ -18,67 +17,44 @@ local QuitDivider = Main:Divider{
    Name = "Quit"
 }
 
--- All functions have the Name, Description and Callback arguments so you can use them whenever ig yeah
-local KillAll = Divider:Button{
-   Name = "Kill all",
-   Description = "Kills all the players in the game!",
-   Callback = function()
-       print("All players killed.")
-   end
-}
+-- Configurações
+local aimbotAtivado = false
+local espAtivado = false
+local corInimigos = Color3.new(1, 0, 0) -- Vermelho
+local corAmigos = Color3.new(0, 0, 1) -- Azul
 
-local LoopKillAll = Divider:Toggle{
-   Name = "Loop kill all",
-   Description = "Loop kills everyone in the game.",
-   Callback = function(State)
-       print("Kill state: ", State)
-   end
-}
+-- Função para alternar o aimbot
+function alternarAimbot()
+    aimbotAtivado = not aimbotAtivado
+    print("Aimbot ativado: " .. tostring(aimbotAtivado))
+end
 
-local OtherToggleStyle = Divider:Toggle{
-   Name = "2nd style of toggle",
-   Style = 2
-}
+-- Função para alternar o ESP
+function alternarESP()
+    espAtivado = not espAtivado
+    print("ESP ativado: " .. tostring(espAtivado))
+end
 
-local Players = Divider:Dropdown{
-   Name = "Player list",
-   Options = {"Player1", "Player2", "Player3", "Player4", "Player5"},
-   Callback = function(Value)
-       print(Value)
-   end
-}
+-- Função para desenhar o ESP
+function desenharESP(jogador, cor)
+    -- Lógica para desenhar o ESP (implemente conforme necessário)
+    print("Desenhando ESP para " .. jogador.Name .. " na cor " .. tostring(cor))
+end
 
-Divider:ColorPicker{
-   Name = "ESP color",
-   Default = Color3.fromRGB(0, 255, 255), -- default,
-   Callback = function(Value)
-       print(Value)
-   end
-}
+-- Exemplo de uso
+alternarAimbot()
+alternarESP()
 
-Divider:Box{
-   Name = "Car name",
-   ClearText = true, -- whether the textbox clears on focus or not
-   Callback = function(Value)
-       print(Value)
-   end
-}
+-- Simulação de detecção de inimigos e amigos
+local inimigo = game.Players.LocalPlayer
+local amigo = game.Players:GetPlayers()[1]
 
-Divider:SearchDropdown{
-   Name = "Teleports",
-   Options = {"Pleasant Park", "Loot Lake", "Tomato Town", "Wailing Woods", "Anarchy Acres", "Retail Row"},
-   ClearText = false, -- default
-   Callback = function(Value)
-       print(Value)
-   end
-}
+if aimbotAtivado then
+    -- Lógica do aimbot (implemente conforme necessário)
+    print("Aimbot mirando em " .. inimigo.Name)
+end
 
-local Quit = QuitDivider:Button{
-   Name = "Closes the ui library.",
-   Callback = function()
-       UI:Quit{
-           Message = "Fuck off...", -- closing message
-           Length = 1 -- seconds the closing message shows for
-       }
-   end
-}
+if espAtivado then
+    desenharESP(inimigo, corInimigos)
+    desenharESP(amigo, corAmigos)
+end
